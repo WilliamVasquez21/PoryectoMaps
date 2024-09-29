5c7c5689464cebc35712e1a4b64fef88ff79f770
steven <43816687+medranosteven@users.noreply.github.com>
2024-09-29 00:39:13 -0600

    mod-minerva-blade

--- resources/views/minerva.blade.php
+++ resources/views/minerva.blade.php
@@ -37,8 +37,12 @@
     @endif
 </div>
 
-<!-- Sección con las tarjetas -->
-<div class="section-container">
+@extends('layouts.app')
+
+@section('title', 'Minerva Maps UES-FMO')
+
+@section('content')
+    <!-- Sección con las tarjetas -->
     @foreach($departments as $department => $cards)
         <div class="section-container" id="{{ strtolower(str_replace(' ', '', $department)) }}">
             <div class="section-title">{{ $department }}</div>
@@ -74,52 +78,53 @@
             @endif
         </div>
     @endforeach
-</div>
 
-<!-- Galería de Imágenes -->
-<div class="container">
-  <div class="image-grid">
-    @foreach ($images as $index => $image)
-      @if ($index == 0)
-        <!-- Primera imagen más grande -->
-        <img class="main-image" src="{{ $image['url'] }}" alt="{{ $image['caption'] }}" />
-      @else
-        <!-- Imágenes secundarias en grid -->
-        <img class="grid-image" src="{{ $image['url'] }}" alt="{{ $image['caption'] }}" />
-      @endif
-    @endforeach
-    
-    <!-- Botón flotante sobre la última imagen del grid -->
-    <div class="button-box" onclick="location.href='{{ route('minerva-overley') }}'">
-      <div class="button-text">Mostrar todas las fotos</div>
-    </div>
-  </div>
-  
-  <div class="container">
-    <!-- Contenedor de imagen destacada y texto -->
-    @foreach ($highlightedImages as $image)
-        <div class="highlighted-container">
-            <div class="info-box">
-                <div class="auditorio-text">{{ $image['title'] }}</div>
-                <div class="location">
-                    <i class="bi bi-geo-alt" style="font-size: 24px;"></i>
-                    <div class="location-text">{{ $image['location'] }}</div>
-                </div>
-                <div class="address">
-                    <i class="bi bi-map" style="font-size: 24px;"></i>
-                    <div class="address-text">{{ $image['address'] }}</div>
-                </div>
-                <div class="capacity">
-                    <i class="bi bi-people" style="font-size: 24px;"></i>
-                    <div class="capacity-text">{{ $image['capacity'] }}</div>
-                </div>
-            </div>
-            <img class="highlighted-image" src="{{ $image['url'] }}" alt="{{ $image['title'] }}" />
+    <!-- Galería de Imágenes -->
+    <div class="container">
+      <div class="image-grid">
+        @foreach ($images as $index => $image)
+          @if ($index == 0)
+            <!-- Primera imagen más grande -->
+            <img class="main-image" src="{{ $image['url'] }}" alt="{{ $image['caption'] }}" />
+          @else
+            <!-- Imágenes secundarias en grid -->
+            <img class="grid-image" src="{{ $image['url'] }}" alt="{{ $image['caption'] }}" />
+          @endif
+        @endforeach
+        
+        <!-- Botón flotante sobre la última imagen del grid -->
+        <div class="button-box" onclick="location.href='{{ route('minerva-overley') }}'">
+          <div class="button-text">Mostrar todas las fotos</div>
         </div>
-    @endforeach
-  </div>
-</div>
-<br><br><br>
+      </div>
+      
+      <div class="container">
+        <!-- Contenedor de imagen destacada y texto -->
+        @foreach ($highlightedImages as $image)
+            <div class="highlighted-container">
+                <div class="info-box">
+                    <div class="auditorio-text">{{ $image['title'] }}</div>
+                    <div class="location">
+                        <i class="bi bi-geo-alt" style="font-size: 24px;"></i>
+                        <div class="location-text">{{ $image['location'] }}</div>
+                    </div>
+                    <div class="address">
+                        <i class="bi bi-map" style="font-size: 24px;"></i>
+                        <div class="address-text">{{ $image['address'] }}</div>
+                    </div>
+                    <div class="capacity">
+                        <i class="bi bi-people" style="font-size: 24px;"></i>
+                        <div class="capacity-text">{{ $image['capacity'] }}</div>
+                    </div>
+                </div>
+                <img class="highlighted-image" src="{{ $image['url'] }}" alt="{{ $image['title'] }}" />
+            </div>
+        @endforeach
+      </div>
+    </div>
+    <br><br><br>
+@endsection
+
 <!-- Footer al final del contenido -->
 <div class="footer">
   <div class="footer-text">© Realizado por estudiantes de Ingeniería en Sistemas Informáticos 2024.</div>
