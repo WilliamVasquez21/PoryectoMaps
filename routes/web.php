28f9b9fb04bbe6a11a0415905a0f7c668721c802
steven <43816687+medranosteven@users.noreply.github.com>
2024-09-29 00:45:12 -0600

    mod-web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MinervaController;
 use App\Http\Controllers\MinervaLaController;
 use App\Http\Controllers\MinervaOverlayController;
+use Illuminate\Support\Facades\Routes;
 
 Route::get('/', [MinervaController::class, 'index'])->name('minerva.home');
 
@@ -14,7 +15,11 @@ Route::get('/minerva-la', [MinervaLaController::class, 'index'])->name('minerva-
 Route::get('/minerva-overley', [MinervaOverlayController::class, 'index'])->name('minerva-overley');
 
 // Ruta para el método index (ya existente)
+// Ruta para Zonas
 Route::get('/zonas', [MinervaController::class, 'index'])->name('minerva.home');
 
 // Nueva ruta para aulas
+// Ruta para Aulas
 Route::get('/aulas', [MinervaController::class, 'getAulas'])->name('aulas.index');
+
+
