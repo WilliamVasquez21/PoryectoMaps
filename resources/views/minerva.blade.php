d9ea4f67f70ae4eda1ee58fc8edd19d3c45230d4
WilliamAlfredo <av906761@gmail.com>
2024-10-03 01:08:05 -0600

    modal sin linea debajo de imagen

--- resources/views/minerva.blade.php
+++ resources/views/minerva.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-
+<link rel="stylesheet" href="{{ asset('css/minerva.css') }}">  
 <!-- Sección con las tarjetas -->
 <div class="container-fluid">
     <div class="row m-3">
