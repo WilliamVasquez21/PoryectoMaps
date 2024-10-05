4439b8f5ab86aca70a9cbccc5c30df4bac47c9b4
Hieloeston235 <117106985+Hieloeston235@users.noreply.github.com>
2024-10-05 11:47:12 -0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
 
 
 
-        if(env( key: 'APP_ENV') === 'production') {
+        if(env( key: 'APP_ENV') === 'local') {
             URL::forceScheme(scheme: 'https');
           }
 
